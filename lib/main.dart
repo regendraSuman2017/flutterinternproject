a5c1b01ca8fd2baf6a5a5bef58782617f3bd860f
Shailja <shailjachauhan.1036@gmail.com>
2022-12-27 10:24:36 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo Nerw',
       theme: ThemeData(
         // This is the theme of your application.
         //
